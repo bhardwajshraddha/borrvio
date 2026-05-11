dae2abeb7c18f5729f028b346343e33a7fc0b142
bhardwajshraddha <bhardwajshradha800@gmail.com>
2026-05-12 01:46:01 +0530

    fix: CORS policy for production

--- server/index.js
+++ server/index.js
@@ -15,7 +15,17 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const path = require("path");
 connectDB();
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://borrvio.vercel.app/",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
